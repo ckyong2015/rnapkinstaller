5c3e5b276ba181fdac28fbdbb4604b62d3bbb120
CK Yong <ckyong2010@gmail.com>
2019-12-09 21:13:54 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A react-native apk installer for Android. This project is based on react-native-apk-installer
---
+++ index.js
@@ -0,0 +1,3 @@
+import { NativeModules } from 'react-native';
+const { ApkInstaller } = NativeModules;
+export default ApkInstaller;
