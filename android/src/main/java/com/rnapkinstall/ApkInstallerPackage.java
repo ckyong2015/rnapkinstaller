334e401c26ac0a018d99a1da1874786f5d894a24
CK Yong <ckyong2010@gmail.com>
2019-12-10 10:55:40 +0800

    change package java

--- android/src/main/java/com/rnapkinstall/ApkInstallerPackage.java
+++ android/src/main/java/com/rnapkinstall/ApkInstallerPackage.java
@@ -13,7 +13,7 @@ import com.facebook.react.bridge.JavaScriptModule;
 public class ApkInstallerPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNApkInstallerNModule(reactContext));
+      return Arrays.<NativeModule>asList(new ApkInstallerModule(reactContext));
     }
 
     // Deprecated from RN 0.47
